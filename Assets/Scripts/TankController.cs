53489ae6b85bda9a52ca1d4b86dd9037e5f868ca
John Winski <JWINSKI@nait.ca>
2025-08-26 19:16:34 -0600

    Added turret rotation

--- Assets/Scripts/TankController.cs
+++ Assets/Scripts/TankController.cs
@@ -1,11 +1,18 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class TankController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    float movementSpeed = 3.0f;
+    float rotationSpeed = 100.0f;
+
+    [SerializeField] InputAction moveAction, rotateAction;
+
+    [SerializeField] Vector2 moveValue, rotateValue;
+
+    [SerializeField] GameObject turret;
+
+    private void Start()
     {
         
     }
@@ -14,5 +21,26 @@ public class TankController : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        // move the object based on the values of the gamepad
+        transform.Translate(Vector3.forward * moveValue.y * movementSpeed * Time.fixedDeltaTime);
+        transform.Rotate(Vector3.up, moveValue.x * rotationSpeed * Time.fixedDeltaTime);
+        if (rotateValue.x !=0 || rotateValue.y !=0 )
+        {
+            turret.transform.LookAt(transform.position + new Vector3(rotateValue.x, turret.transform.localPosition.y, rotateValue.y));
+        }        
+    }
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveValue = context.ReadValue<Vector2>();
+    }
+
+    public void OnRotate(InputAction.CallbackContext context)
+    {
+        rotateValue = context.ReadValue<Vector2>();
     }
 }
