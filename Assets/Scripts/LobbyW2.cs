834eb4b6af24c012812fd48b739c420bdc8ba40f
John Winski <JWINSKI@nait.ca>
2025-09-02 20:32:02 -0600

    Added a different color to each player tank.

--- Assets/Scripts/LobbyW2.cs
+++ Assets/Scripts/LobbyW2.cs
@@ -17,6 +17,8 @@ public class LobbyW2 : MonoBehaviour
 
     [SerializeField] GameObject[] spawnLocations;
 
+    Color[] tankColors = {Color.red, Color.blue, Color.green, Color.yellow};
+
     // Start is called before the first frame update
     void Start()
     {
@@ -97,6 +99,7 @@ public class LobbyW2 : MonoBehaviour
             PlayerInput player = playerInputManager.JoinPlayer(i, -1, null,  Gamepad.all[index]);
             player.transform.position = spawnLocations[i].transform.position;
             player.GetComponent<TankController>().SetPlayerID(i);
+            player.GetComponentInChildren<MeshRenderer>().material.color = tankColors[i];
             gameManager.AddTank(player.gameObject);            
         }
         MyEvents.ActivateScores.Invoke(gamepadManager.PlayerCount());
